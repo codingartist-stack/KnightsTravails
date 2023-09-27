14892538c45750a3f178e7ddd93874fdba5ce11d
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-09-27 10:30:09 -0700

    using a other submission as guide. using a map

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-const createBoard = (boardSize = 8) => {
-  let board = [];
-  for (let i = 0; i < boardSize; i++) {
-    let row = [];
-    board.push(row);
-    for (let j = 0; j < boardSize; j++) {
-      let column = 0;
-      row.push(column);
+const Chess = (boardSize = 8) => {
+  const chessBoard = new Map();
+
+  const createBoard = (boardSize) => {
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        chessBoard.set(`${[i, j]}`, []);
+      }
     }
-  }
-  console.log(board);
+  };
+
+  createBoard(boardSize);
+  const createEdges = (board = chessBoard) => {
+    for (const [position] of board) {
+      const positionArray = position.split(',');
+      const x = parseInt(positionArray[0]);
+      const y = parseInt(positionArray[1]);
+
+      const movePattern = {
+        1: [x + 1, y + 2],
+        2: [x + 2, y + 1],
+        4: [x + 2, y - 1],
+        5: [x + 1, y - 2],
+        7: [x - 1, y - 2],
+        8: [x - 2, y - 1],
+        10: [x - 2, y + 1],
+        11: [x - 1, y + 2],
+      };
+      for (const pattern in movePattern) {
+        const move = movePattern[pattern].toString();
+        if (board.has(move) && !board.get(position).includes(move)) {
+          chessBoard.get(position).push(move);
+        }
+      }
+    }
+  };
+
+  createEdges();
+  console.log(chessBoard);
+
+  return {};
 };
 
-createBoard();
+Chess();
